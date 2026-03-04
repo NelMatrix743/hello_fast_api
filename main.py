160b945492fcd4b80b555682acd6c5ce93091319
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-03-04 17:00:38 +0000

    core main.py code snippet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def main():
-    print("Hello from hello-fast-api!")
+import uvicorn
 
 
 if __name__ == "__main__":
-    main()
+    
+    uvicorn.run("src.app:app", host="0.0.0.0", port=7000, reload=True)
