0f3c2335deecdd619fbd9c3ea296405e0a9b6fc8
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-03-04 18:07:46 +0000

    fix: make required changes

--- src/db.py
+++ src/db.py
@@ -35,7 +35,7 @@ async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 async def create_db_and_tables():
     async with engine.begin() as conn:
-        await conn.run_sync(DeclarativeBase.metadata.create_all)
+        await conn.run_sync(Base.metadata.create_all)
 
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
