cb4d8ed4ab6b415a5f6b4b1e49567a89ad795afd
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-03-09 20:59:11 +0000

    add signature for file upload request handler

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, File, UploadFile, Form, Depends
 from src.db import Post, create_db_and_tables, get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
@@ -11,3 +11,12 @@ async def lifespan(app: FastAPI):
 
 app: FastAPI = FastAPI(lifespan=lifespan)
 
+
+
+# API endpoints
+
+@app.post("/upload")
+async def upload_files(file: UploadFile = File(...),
+                       caption: str = Form(""),
+                       session: AsyncSession = Depends(get_async_session)):
+    pass 
