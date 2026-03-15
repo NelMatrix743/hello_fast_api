fb9d4f7904f3ffb31c009dc4101b4e09971e1261
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-03-15 16:42:57 +0000

    feat: implement logic for /feed endpoint to get feed entries in database

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, HTTPException, File, UploadFile, Form, Depends
 from src.db import Post, create_db_and_tables, get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 from contextlib import asynccontextmanager
 from imagekitio import APIStatusError
 from src.media import imagekit_client
@@ -69,6 +70,29 @@ async def upload_file(file: UploadFile = File(...), caption: str = Form(""), ses
                 os.unlink(temporary_uploaded_file_path)
             except PermissionError:
                 pass
+
+
+
+@app.get("/feed")
+async def get_feed(session: AsyncSession = Depends(get_async_session)):
+    result = await session.execute(select(Post).order_by(Post.created_at.desc()))
+    posts = [row[0] for row in result.all()]
+
+    posts_data = []
+    for post in posts:
+        posts_data.append(
+            {
+                "id" : str(post.id),
+                "caption" : post.caption,
+                "url" : post.url,
+                "file_type" : post.file_type,
+                "file_name" : post.file_name,
+                "created_at" : post.created_at.isoformat()
+            }
+        )
+
+    return { "post" : posts_data }
+
 
 
 # eosc
