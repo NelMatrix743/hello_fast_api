3d134aa6b038ffc6405b9406545546dbcd6fbe3a
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-03-16 17:26:29 +0000

    implement a function to get User database table

--- src/db.py
+++ src/db.py
@@ -5,6 +5,7 @@ from sqlalchemy.ext.asyncio.engine import AsyncEngine
 from sqlalchemy.orm import DeclarativeBase, relationship
 
 from fastapi_users.db import SQLAlchemyUserDatabase, SQLAlchemyBaseUserTableUUID
+from fastapi import Depends
 
 import uuid
 from datetime import datetime, timezone
@@ -50,3 +51,10 @@ async def create_db_and_tables():
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
         yield session
+
+
+async def get_user_db(session: AsyncSession = Depends(get_async_session)):
+    yield SQLAlchemyUserDatabase(session, User)
+
+
+# eosc
