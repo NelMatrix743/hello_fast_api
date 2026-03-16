70226574872c133340b33263da21cdc58b44dcaa
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-03-16 18:09:14 +0000

    add schemas.py source file

---
+++ src/schemas.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel
+from fastapi_users import schemas
+import uuid
+
+
+class PostCreate(BaseModel):
+    title: str
+    content: str
+
+
+class PostResponse(BaseModel):
+    title: str
+    content: str
+
+
+class UserCreate(schemas.BaseUserCreate):
+    pass
+
+
+class UserRead(schemas.BaseUser[uuid.UUID]):
+    pass
+
+
+class UserUpdate(schemas.BaseUserUpdate):
+    pass
+
+
+# ESOC
